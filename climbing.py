694827a85fc09f7517cb626b79f86de1a99be752
Julien Castellano <julien@MacBook-Pro-de-Julien.local>
2025-01-23 17:24:40 +0100

    ajout colonne depuis les dicos

--- climbing.py
+++ climbing.py
@@ -3,7 +3,9 @@ import pandas as pd
 import folium
 from streamlit_folium import st_folium
 from folium.plugins import MarkerCluster
+import plotly.express as px
 import ast
+from folium import IFrame
 
 # Fonction pour charger les données
 @st.cache_data
@@ -30,23 +32,28 @@ marker_cluster = MarkerCluster().add_to(map_folium)
 # Carte Folium
 m = folium.Map(location=[44.1, 5.1], zoom_start=12)
 
+columns_to_convert = ["rocks"]
+for col in columns_to_convert:
+    df[col] = df[col].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    
+
 # Boucle pour ajouter les marqueurs
 for index, row in df.iterrows():
-    # Construire le contenu du popup
-    types = row["climbing_types"]  # Transformation de la liste en chaîne
-    levels = row["levels"]
-    exposition = row["west"]
-    saison = row["north"]
-    
+    rocks = ", ".join(row["rocks"]) # Transformation de la liste en chaîne
     popup_html = f"""
     <div style='width:300px'>
     <b>Nom :</b> {row['name']}<br>
-    <b>Type d'escalade :</b> {types}<br>
-    <b>Difficulté :</b> {levels}<br>
-    <b>Exposition :</b> {exposition}<br>
-    <b>Période :</b> {saison}<br>
+    <b>Region :</b> {row['region']}<br>
+    <b>Nombre de voies :</b> {row['route_count']}<br>
+    <b>Roche :</b> {rocks}<br>
+    <img src="{row['photo']}" alt="Photo" width="150">
     </div>
     """
+
+    # Créer un IFrame pour encapsuler le contenu HTML
+    iframe = IFrame(popup_html, width=300, height=200)
+    popup = folium.Popup(iframe, max_width=300)
+
     # Créer le marqueur avec le popup et l'icône personnalisée
     marker = folium.Marker(
         location=[row['latitude'], row['longitude']],
